947698461d14d56831bc3aa4006cd9352833d9bd
JustinLemon <86700378+JustinLemon@users.noreply.github.com>
2021-07-25 21:59:09 +0800

    Update RegisterScreen.js

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -2,19 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button, Image, ImageBackground} from 'react-native';
 
-const RegisterScreen = () => {
+const RegisterScreen = (props) => {
 
+  
   return (
-    <View style={styles.defaultBackground}>
+    <View style={styles.background}>
         <Text>This is RegisterScreen</Text>
+
+        <View style={styles.registrationForm}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  defaultBackground:{
+  background:{
       backgroundColor: "white",
       flex: 1,
+  },
+  registrationForm:{
+
   },
 });
 
