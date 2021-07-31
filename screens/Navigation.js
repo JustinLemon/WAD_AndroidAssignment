41914f2bdb91ff54ca4c573806e17ccc5d9f3ea8
JongJianYi <asdasqweq060@gmail.com>
2021-07-31 21:15:01 +0800

    Add SellItem.Js

--- screens/Navigation.js
+++ screens/Navigation.js
@@ -8,6 +8,7 @@ import { createAppContainer } from "react-navigation";
 import WelcomeScreen from "./WelcomeScreen";
 import RegisterScreen from "./RegisterScreen";
 import UserPageScreen from "./UserPageScreen";
+import SellItemScreen from './SellItem';
 
 const navigator = createStackNavigator(
   {
@@ -26,6 +27,12 @@ const navigator = createStackNavigator(
     },
     UserPage: {
       screen: UserPageScreen,
+      navigationOptions: {
+      headerShown: true,
+    }
+    ,
+    SellItem: {
+      screen: SellItemScreen,
       navigationOptions: {
       headerShown: true,
     }
